e93a7e1af8baf77c9abcadb624e8b22e19ee907d
Sergey Maximov <dev9092336062@gmail.com>
2024-02-28 22:09:53 +0300

    git pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-weather/", // TODO: for git pages
   plugins: [react()],
 });
