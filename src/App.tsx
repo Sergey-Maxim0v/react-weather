4c9a04b1e241cd7ecef3d78d60b46e9a02b65c5e
Sergey Maximov <dev9092336062@gmail.com>
2024-02-28 20:40:19 +0300

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import Widget from "./components/Widget";
 import { WIDGET_SIZE } from "./components/Widget/types";
 import Powered from "./components/Powered";
+import { useState } from "react";
 
 function App() {
+  const [size, setSize] = useState<WIDGET_SIZE>(WIDGET_SIZE.medium);
+
   return (
     <div className="app">
+      <div className="buttons">
+        <button onClick={() => setSize(WIDGET_SIZE.small)} className="button">
+          Small
+        </button>
+        <button onClick={() => setSize(WIDGET_SIZE.medium)} className="button">
+          Medium
+        </button>
+        <button onClick={() => setSize(WIDGET_SIZE.large)} className="button">
+          Large
+        </button>
+      </div>
+
       <Widget size={WIDGET_SIZE.small} />
-      {/*<Widget size={WIDGET_SIZE.medium} />*/}
-      {/*<Widget size={WIDGET_SIZE.large} />*/}
+      <Widget size={size} />
+      <Widget size={WIDGET_SIZE.large} />
 
       <div className="powered__row">
         <Powered className="powered" />
